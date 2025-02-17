708aae620eb78ab7758ce9ef3f663749f0aa671e
atrillini <152901272+atrillini@users.noreply.github.com>
2025-02-17 15:18:42 +0100

    Update setStatus.py

--- setStatus.py
+++ setStatus.py
@@ -3,7 +3,34 @@ import pandas as pd
 import mysql.connector
 from sh import Sh
 
-print('il mio primo script python')
+
+cfg2 = {
+        'stock_url': 'https://oldpremiata.pilcommunication.com/4d/stock/stock.csv',
+        'mysql': {
+            'host': 'crossover.proxy.rlwy.net', 
+            'db_user': 'root', 
+            'db_password': 'ICReplqqhsfqsimGVguywqGrBtvVWAWJ',
+            'db_name': 'railway', 
+            'port':'31393',
+            'products_table': 'premiata_products', 
+            'stocks_table': 'premiata_stocks',
+        }
+    }
+
+def db_connect(mysql_cfg):
+    return mysql.connector.connect(
+        user=mysql_cfg['db_user'],
+        password=mysql_cfg['db_password'],
+        host=mysql_cfg['host'],
+        port=mysql_cfg['port'],
+        database=mysql_cfg['db_name']
+    )
+
+dbconn = db_connect(cfg2['mysql'])
+cur = dbconn.cursor(buffered=True)
+print('provo a fare la connessione al db')
+print(dbconn)
+
 
 
     
