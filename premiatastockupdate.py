860d3f78b7dd303ed096a49d00b7c543998d1f53
atrillini <152901272+atrillini@users.noreply.github.com>
2025-03-20 10:13:18 +0100

    Update premiatastockupdate.py

--- premiatastockupdate.py
+++ premiatastockupdate.py
@@ -95,7 +95,7 @@ def map_stocks(stock_data_processed,db_cursor):
 def read_stocks_file(file_path):
     return pd.read_csv(file_path, sep=',')
 
-def dbConnect(mysql_cfg):
+def db_connect(mysql_cfg):
     return mysql.connector.connect(
         user=mysql_cfg['db_user'],
         password=mysql_cfg['db_password'],
